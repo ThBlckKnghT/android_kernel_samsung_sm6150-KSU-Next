15f09f4e6f61b84368c3f1fde76bd27e9de3c135
ThBlckKnghT <roichannafis2@gmail.com>
2025-04-30 19:56:57 +0700

    add support ksu 12495 (1.0.6) and susfs 1.5.7

--- fs/notify/inotify/inotify_fsnotify.c
+++ fs/notify/inotify/inotify_fsnotify.c
@@ -34,6 +34,10 @@
 
 #include "inotify.h"
 
+u32 inotify_mark_user_mask(struct fsnotify_mark *mark)
+{
+    return mark->mask & IN_ALL_EVENTS;
+}
 /*
  * Check if 2 events contain the same information.
  */
